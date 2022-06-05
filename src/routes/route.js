e7b2096c70cf1d7cabb940aad090516a21e5b1a6
satyajitmalik <satyajtmalik@gmail.com>
2022-06-05 12:50:20 +0530

    updated route_mongodb

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,10 +8,6 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
-
 router.post("/createBook",UserController.createBook)
 
 router.get("/getBookData",UserController.getBookData)
